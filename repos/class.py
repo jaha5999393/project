51d9433f3fca3dda22c0814c248674586580f497
jaha5999393 <101399996+jaha5999393@users.noreply.github.com>
2022-03-13 14:12:24 +0500

    Create class.py

---
+++ repos/class.py
@@ -0,0 +1,117 @@
+# class Info:
+#
+#     def __init__(self, ism, familiya, tyil, login, password, ):
+#         self.ism = ism
+#         self.familiya = familiya
+#         self.tyil = tyil
+#         self.pas = password
+#         self.log = login
+#
+#     def get_name(self):
+#         return self.ism
+#
+#     def get_lastname(self):
+#         return self.familiya
+#
+#     def get_fullname(self):
+#         return f"{self.ism} {self.familiya}"
+#
+#     def tanishtir(self):
+#         print(f"Ismim {self.ism} {self.familiya}. {self.tyil} yilda tu'gilganman")
+#
+#     def get_age(self):
+#         return self.tyil
+#
+#     def get_login(self):
+#         return self.log
+#
+#     def get_password(self):
+#         return self.pas
+# #
+# #
+# fulname = Info("Jahongir", "Oripov", 1993, "123456", "dragan11")
+# print(fulname.get_fullname())
+# print(fulname.get_age())
+# print(fulname.get_login())
+# print(fulname.get_password())
+
+
+# class Hayvon:
+#     def __init__(self, ovoz, ovqat, harakat):
+#         self.ovozz = ovoz
+#         self.ovqatt = ovqat
+#         self.hara = harakat
+#
+#     def get_ovoz(self):
+#         return self.ovozz
+#
+#     def get_ovqat(self):
+#         return self.ovqatt
+#
+#     def get_har(self):
+#         return self.hara
+# #
+# wolf = Hayvon("Qo'rqinchli", "Go'sht", "Tezkor")
+# print(wolf.get_ovoz())
+# print(wolf.get_ovqat())
+# print(wolf.get_har())
+
+
+# class Harakat:
+#     def __init__(self, onga, chapga, orqaga, oldinga, jami):
+#         self.ong = onga
+#         self.chap = chapga
+#         self.orqa = orqaga
+#         self.oldin = oldinga
+#         self.jam = jami
+#     def get_ong(self):
+#         self.jam += self.ong
+#         return self.ong
+#     def get_chap(self):
+#         self.jam += self.chap
+#         return self.chap
+#     def get_orqa(self):
+#         self.jam -= self.orqa
+#         return self.orqa
+#     def get_oldin(self):
+#         self.jam += self.oldin
+#         return self.oldin
+#
+# qadam = Harakat(int(input()))
+# print(qadam.jam)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
